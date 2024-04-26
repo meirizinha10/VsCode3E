a33dfe013d0b445177b8cb5ea3ee27551d4fdb13
meirizinha10 <161828550+meirizinha10@users.noreply.github.com>
2024-04-26 07:38:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-const botoes = document. querySelectorAll(" .botao");
-console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+for(let i=0;i<botoes.length;i++){
+    botoes [i].onclick=function(){
+        botoes [i].classList.add ("ativo");
+    }
+}
